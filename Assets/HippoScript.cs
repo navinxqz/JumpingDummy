4bb2c0d7bf2c98b6405ca0a55b395702f8494de0
Navin <navinmdnawshin@gmail.com>
2025-02-11 01:50:21 +0600

    asset

---
+++ Assets/HippoScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class HippoScript : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        gameObject.name = "Hippo";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) == true) { rb.linearVelocity = Vector2.up * 10; }
+            
+    }
+}
